65cf15516065e3a5e121278b0157fb40c22aaa50
Marshal Horn <kamocat@gmail.com>
2026-02-24 20:06:09 -0800

    Async Busio: Nordic port

---
+++ ports/nordic/common-hal/busio/dma.h
@@ -0,0 +1,9 @@
+// This file is part of the CircuitPython project: https://circuitpython.org
+//
+// SPDX-FileCopyrightText: Copyright (c) 2026
+//
+// SPDX-License-Identifier: MIT
+
+#pragma once
+
+#include "shared-bindings/busio/dma.h"
